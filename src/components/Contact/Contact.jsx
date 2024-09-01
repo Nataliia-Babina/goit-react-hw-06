5f2de17f5857aab0a693baf4ccf6cf1612627ae7
Nataliia Babina <157317342+Nataliia-Babina@users.noreply.github.com>
2024-09-01 21:37:28 +0200

    HW-6-react

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,15 @@
+import { useDispatch } from "react-redux";
+import { deleteContact } from '../../redux/contactsSlice';
 import css from "./Contact.module.css";
 import { FaUser, FaPhone } from "react-icons/fa";
 
-const Contact = ({ contact, onDelete }) => {
+const Contact = ({ contact }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = (id) => {
+    dispatch(deleteContact(id));
+  };
+
   return (
     <div className={css.card}>
       <div className={css.wrapper}>
@@ -12,7 +20,7 @@ const Contact = ({ contact, onDelete }) => {
           <FaPhone /> {contact.number}
         </p>
       </div>
-      <button className={css.button} onClick={() => onDelete(contact.id)}>
+      <button className={css.button} onClick={() => handleDelete(contact.id)}>
         Delete
       </button>
     </div>
